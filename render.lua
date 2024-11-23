87ff2c8fdbba4ff54dfdc764958e6068de404a19
dyphire <qimoge@gmail.com>
2024-11-23 12:50:15 +0800

    feat: add `save_danmaku` option to save danmaku

--- render.lua
+++ render.lua
@@ -267,7 +267,21 @@ mp.add_hook("on_unload", 50, function()
     local rm4 = utils.join_path(danmaku_path, "temp.mp4")
     local rm5 = utils.join_path(danmaku_path, "bahamut.json")
     if file_exists(rm1) then os.remove(rm1) end
-    if file_exists(rm2) then os.remove(rm2) end
+    if file_exists(rm2) then
+        if options.save_danmaku then
+            local path = mp.get_property("path")
+            if not path or is_protocol(path) then return end
+            local dir = get_parent_directory(path)
+            local filename = mp.get_property('filename/no-ext') 
+            local danmaku_out = utils.join_path(dir, filename .. ".xml")
+            if file_exists(danmaku_out) then return end
+            convert_with_danmaku_factory(rm2, danmaku_out)
+            if file_exists(danmaku_out) then
+                msg.verbose("成功保存弹幕文件到视频文件同目录")
+            end
+        end
+        os.remove(rm2)
+    end
     if file_exists(rm3) then os.remove(rm3) end
     if file_exists(rm4) then os.remove(rm4) end
     if file_exists(rm5) then os.remove(rm5) end
