26d27105a48b3712cf3448a353dca60f3eab4ca0
Misaka <142686220+MiKoto-Railgun@users.noreply.github.com>
2025-01-27 23:47:34 +0800

    refactor: Optimize the style modification menu and add a reset button

--- main.lua
+++ main.lua
@@ -167,7 +167,7 @@ function open_input_menu_uosc()
 
     if danmaku.anime and danmaku.episode then
         items[#items + 1] = {
-            hint = string.format("已关联弹幕：%s-%s", danmaku.anime, danmaku.episode),
+            hint = string.format("  已关联弹幕：%s", danmaku.anime),
             keep_open = true,
             selectable = false,
         }
